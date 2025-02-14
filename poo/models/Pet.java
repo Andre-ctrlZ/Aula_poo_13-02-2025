50e97ba6b84e9ab2e225d0060773af7c45376633
Andre-ctrlZ <luiace2003@gmail.com>
2025-02-14 00:44:18 +0000

    Adicionado as variáveis e pet.

--- poo/models/Pet.java
+++ poo/models/Pet.java
@@ -1,5 +1,49 @@
 package poo.models;
 
 public class Pet {
+    String nome;
+    String raça;
+    String porte;
+    String alimentação;
 
+    public Pet(String nome, String raça, String porte, String alimentação, int idade) {
+        this.nome = nome;
+        this.raça = raça;
+        this.porte = porte;
+        this.alimentação = alimentação;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getRaça() {
+        return raça;
+    }
+    public void setRaça(String raça) {
+        this.raça = raça;
+    }
+    public String getPorte() {
+        return porte;
+    }
+    public void setPorte(String porte) {
+        this.porte = porte;
+    }
+    public String getAlimentação() {
+        return alimentação;
+    }
+    public void setAlimentação(String alimentação) {
+        this.alimentação = alimentação;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    int idade;
+   
 }
