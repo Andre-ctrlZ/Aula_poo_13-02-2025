e87eea55f7798e01f5477a152372a60554fb02c8
Andre-ctrlZ <luiace2003@gmail.com>
2025-03-06 23:36:16 +0000

    atividade não feita.

---
+++ Atividade_1/Funcionario.java
@@ -0,0 +1,9 @@
+public abstract class Funcionario {
+    protected String nome;
+    protected String DataNascimento;
+    protected String Sexo;
+    protected String Setor;
+    protected Double SalarioBase;
+
+    
+}
