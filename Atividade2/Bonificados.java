390224c7f4458764722eabdc806a93f7df1e52d1
Andre-ctrlZ <luiace2003@gmail.com>
2025-03-08 00:44:38 +0000

    Correção, faltam as mesmas implementações.

--- Atividade2/Bonificados.java
+++ Atividade2/Bonificados.java
@@ -1,6 +1,15 @@
 package Atividade2;
 
 public enum Bonificados {
-    GERENTE ,
-    DIRETOR;
+    GERENTE(1.3),
+    DIRETOR(1.4);
+    private final double value;
+
+    Bonificados(double value) {
+        this.value = value;
+    }
+
+    public double getValue() {
+        return value;
+    }
 }
